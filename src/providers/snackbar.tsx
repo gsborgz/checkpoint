19a69ff432fcd427146b13ca99207366fddb81aa
gsborgz <borgesgabrielsilva@gmail.com>
2024-05-17 01:50:46 -0300

    refactor: making easier to show error messages from `try catch`

--- src/providers/snackbar.tsx
+++ src/providers/snackbar.tsx
@@ -4,12 +4,12 @@ import { createContext, useState } from 'react';
 import Snackbar, { SnackbarTheme } from '@/components/snackbar';
 import { useLocale } from '@/hooks/locale';
 
-
 export type SnackbarData = {
   snackbarText: string;
   snackbarTheme: SnackbarTheme;
   openSnackbar: boolean;
   showMessage: (message: string, args?: Record<string, any>, theme?: SnackbarTheme, duration?: number) => void;
+  showErrorMessage: (message: string) => void;
 };
 
 export const SnackbarContext = createContext({} as SnackbarData);
@@ -20,7 +20,12 @@ export default function SnackbarProvider({ children }: { children: React.ReactNo
   const [open, setOpen] = useState<boolean>(false);
   const { locale } = useLocale();
 
-  function showMessage(key: string, args?: Record<string, any>, theme: SnackbarTheme = SnackbarTheme.Normal, duration: number = 5000) {
+  function showMessage(
+    key: string,
+    args?: Record<string, any>,
+    theme: SnackbarTheme = SnackbarTheme.Normal,
+    duration: number = 5000,
+  ) {
     setText(locale(key, args));
     setTheme(theme);
     setOpen(true);
@@ -30,6 +35,19 @@ export default function SnackbarProvider({ children }: { children: React.ReactNo
     }, duration);
   }
 
+  function showErrorMessage(message: string) {
+    if (message.startsWith('{')) {
+      const messageObj = JSON.parse(message);
+
+      if (messageObj?.key) {
+        showMessage(messageObj.key, messageObj.args, SnackbarTheme.Error);
+      }
+    } else {
+      // TODO: Retirar log
+      console.error(message);
+    }
+  }
+
   return (
     <SnackbarContext.Provider
       value={{
@@ -37,6 +55,7 @@ export default function SnackbarProvider({ children }: { children: React.ReactNo
         snackbarTheme: theme,
         openSnackbar: open,
         showMessage,
+        showErrorMessage,
       }}
     >
       {children}
