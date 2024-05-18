c5c6433e8676ffa4e587ddfcf1a822a9ef2ce1eb
gsborgz <borgesgabrielsilva@gmail.com>
2024-05-18 01:07:20 -0300

    chore: organizing tranlation keys

--- src/assets/i18n/status.ts
+++ src/assets/i18n/status.ts
@@ -1,37 +1,37 @@
 export const status = {
-  title: {
-    en: 'Status',
-    fr: 'Statut',
-    ptbr: 'Status',
-  },
-  updated_at: {
-    en: 'Updated at',
-    fr: 'Mis à jour le',
-    ptbr: 'Atualizado em',
+  database: {
+    en: 'Database',
+    fr: 'Base de données',
+    ptbr: 'Banco de dados'
   },
   dependencies: {
     en: 'Dependencies',
     fr: 'Dépendances',
-    ptbr: 'Dependências',
-  },
-  database: {
-    en: 'Database',
-    fr: 'Base de données',
-    ptbr: 'Banco de dados',
-  },
-  version: {
-    en: 'Version',
-    fr: 'Version',
-    ptbr: 'Versão',
+    ptbr: 'Dependências'
   },
   max_connections: {
     en: 'Max connections',
     fr: 'Connexions max',
-    ptbr: 'Conexões máximas',
+    ptbr: 'Conexões máximas'
   },
   opened_connections: {
     en: 'Opened connections',
     fr: 'Connexions ouvertes',
-    ptbr: 'Conexões abertas',
+    ptbr: 'Conexões abertas'
   },
+  title: {
+    en: 'Status',
+    fr: 'Statut',
+    ptbr: 'Status'
+  },
+  updated_at: {
+    en: 'Updated at',
+    fr: 'Mis à jour le',
+    ptbr: 'Atualizado em'
+  },
+  version: {
+    en: 'Version',
+    fr: 'Version',
+    ptbr: 'Versão'
+  }
 };
